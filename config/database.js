fdaf330034eb2aaf6358e27b974db6eb34eb1d8f
Ingrid Beatriz Silva <108553706+ingrid-beatriiz@users.noreply.github.com>
2026-05-03 22:42:14 +0000

    fix: adiciona configuracoes para resolver erro de pacote pg no vercel

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  native: false,
   dialectOptions: {
     ssl: {
       require: true,
