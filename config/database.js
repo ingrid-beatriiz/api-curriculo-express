99ea050a9059aa5daf00999fc05d1654d02d7602
Ingrid Beatriz Silva <108553706+ingrid-beatriiz@users.noreply.github.com>
2026-05-03 22:48:18 +0000

    fix: remove pg-native e utiliza dialectModule no sequelize

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const pg = require('pg'); 
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  native: false,
+  dialectModule: pg, 
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false 
+      rejectUnauthorized: false
     }
   }
 });
